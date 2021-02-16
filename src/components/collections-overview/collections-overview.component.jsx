5ab6307b00eef2a1ace7db9bd68110ab69cb3d28
dav1dborja <davidborjamartinez@gmail.com>
2021-02-16 23:57:09 +0100

    started state normalization by wrapping our shop data in an object and modify our shop selector

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,7 +5,7 @@ import {createStructuredSelector} from 'reselect';
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import {selectCollections} from '../../redux/shop/shop.selectors'
 
-// import '.collections-overview.styles.scss';
+import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,6 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -15,9 +9,8 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-export const selectCollection = (collectionUrlParam) =>
+export const selectCollection = memoize((collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-  );
+    collections[collectionUrlParam]
+  )
+);
