025b16935d10219c89c8ae480db2274e56e2a626
dav1dborja <davidborjamartinez@gmail.com>
2021-02-17 17:58:54 +0100

    turning the collections object into an array in shop.selectors, updating stripe integration and favicon & title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51ILnCOFmvkLB53vl2pHcpoQTe33LnYAEEMQiQE74mJAzoWxdYIFgNUsCytTaSfprDciQpohz7l9MATyKeQObMKIh00d1Sa9Sy0";
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (<StripeCheckout
+    label="Pay Now"
+    name="CROWN Clothing Ltd."
+    billingAddress
+    shippingAddress
+    image="https://svgshare.com/i/CUz.svg"
+    description={`Your total is $${price}`}
+    amount={priceForStripe}
+    panelLabel='Pay Now'
+    token={onToken}
+    stripeKey={publishableKey}
+  />)
+}
+
+export default StripeCheckoutButton;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,8 +9,14 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections[collectionUrlParam]
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   )
 );
