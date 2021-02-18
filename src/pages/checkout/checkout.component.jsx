f1cd48c0a89e2e7ed9d8dd87e74c954a01a87462
dav1dborja <davidborjamartinez@gmail.com>
2021-02-18 16:27:28 +0100

    authorising domain on firebase and removing redux-logger from production build

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -41,7 +41,7 @@ const CheckoutPage = ({ cartItems, total }) => {
         <div className="test-warning">
           *Please use the following test credit card for payments*
           <br />
-          4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+          4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
         </div>
       </div>
       <StripeCheckoutButton price={total} />
